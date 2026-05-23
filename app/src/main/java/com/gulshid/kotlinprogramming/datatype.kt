b7ca3c5f73e10a0565d9b43ac9ff656019cbdd31
Gulshid Zada <gulshidzada4@gmail.com>
2026-05-23 20:06:50 +0500

    datatype done

---
+++ app/src/main/java/com/gulshid/kotlinprogramming/datatype.kt
@@ -0,0 +1,30 @@
+package com.gulshid.kotlinprogramming
+
+fun main(args: Array<String>) {
+    var a : Boolean = true;
+
+    var b : Char = 'H';
+
+    var c : Byte = 12;
+
+    var d : Short = -365;
+
+    var e : Int = 4583;
+
+    var f : Long = -848383L;
+
+    var g : Float = 5.883939F;
+
+    var h : Double = 9.3838838;
+
+
+    println(a);
+    println(b);
+    println(c);
+    println(d);
+    println(e);
+    println(f);
+    println(g);
+    println(h);
+
+}
