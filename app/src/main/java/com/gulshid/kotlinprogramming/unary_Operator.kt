dad2f4f210f3f33e9206913fb96e043ef2cf2abf
Gulshid Zada <gulshidzada4@gmail.com>
2026-05-24 17:06:26 +0500

    unary operator done

---
+++ app/src/main/java/com/gulshid/kotlinprogramming/unary_Operator.kt
@@ -0,0 +1,24 @@
+package com.gulshid.kotlinprogramming
+
+fun main(args: Array<String>) {
+    println("------ Unary Operator -------");
+    var num : Double = 7.6;
+    var ischeck : Boolean = true;
+
+    println("The +Num  : ${+num}");
+    println("The -Num  : ${-num}");
+    println("The ++Num  : ${++num}");
+    println("The --Num  : ${--num}");
+    println("The !ischeck  : ${!ischeck}");
+
+    println("--------------");
+    var n : Double = 4.7;
+    println("n++ ${n++}");
+    println("n ${n}");
+    println("++n ${++n}");
+    println("n ${n}");
+
+
+
+
+}
